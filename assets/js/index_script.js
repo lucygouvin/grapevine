2a217048922cc0f80c1ab40cf0e25242e0a6fa42
lgouvin <gouvin.lucy@gmail.com>
2023-07-18 00:07:41 -0400

    added some polish to modal window data

--- assets/js/index_script.js
+++ assets/js/index_script.js
@@ -7,7 +7,7 @@ var mapNorth = 37.81098;
 var mapWest = -122.483716;
 var mapSouth = 37.732007;
 var mapEast = -122.370076;
-var cityName = "San Francisco"
+var cityName = "San Francisco";
 // INSTANTIATE MAP INSTANCE
 mapboxgl.accessToken =
   "pk.eyJ1IjoibHVjeWdvdXZpbiIsImEiOiJjbGswYzBpN28wNjB5M2tyY3p4N2FkZ2w2In0.j5zYh-z5brFrxATwtomcMg";
@@ -24,19 +24,28 @@ const map = new mapboxgl.Map({
   maxBounds: bounds,
 });
 
-map.on('load', function () {
+map.on("load", function () {
   map.resize();
 });
 
+$(function () {
+  $("#City-Modal").dialog({
+    position: { my: "center bottom-100%", at: "center bottom", of: $("#map") },
+    width: 550,
+    closeText: "X",
+  });
+  $(".ui-dialog").css("display", "none");
+});
 
 // CHANGE MAP VIEW
 // Get new bounds from the button that was clicked
 $("a").on("click", function (e) {
- mapWest = parseFloat(e.target.getAttribute("data-west"));
- mapEast = parseFloat(e.target.getAttribute("data-east"));
- mapSouth = parseFloat(e.target.getAttribute("data-south"));
- mapNorth = parseFloat(e.target.getAttribute("data-north"));
- cityName = e.target.textContent
+  e.preventDefault;
+  mapWest = parseFloat(e.target.getAttribute("data-west"));
+  mapEast = parseFloat(e.target.getAttribute("data-east"));
+  mapSouth = parseFloat(e.target.getAttribute("data-south"));
+  mapNorth = parseFloat(e.target.getAttribute("data-north"));
+  cityName = e.target.textContent;
   var avgLat = (mapSouth + mapNorth) / 2;
   var avgLon = (mapEast + mapWest) / 2;
 
@@ -44,7 +53,6 @@ $("a").on("click", function (e) {
     [mapWest, mapSouth],
     [mapEast, mapNorth],
   ];
-  console.log("🚀 ~ file: index_script.js:33 ~ bounds:", bounds)
   var center = [avgLon, avgLat];
   // Reset the map bounds and pan to it
   map.setMaxBounds(bounds);
@@ -58,23 +66,16 @@ map.on("mousemove", function (e) {
   lat = e.lngLat.lat;
 });
 
-
 // USE LAT AND LON TO GET LOCATION DATA
 $("#map").on("click", function (e) {
-  console.log("lat: " + lat);
-  console.log("lon: " + lon);
-  getAuthToken(); //AuthToken for Amadeus stats, automatidally goes to get safety stats 
+  getAuthToken(); //AuthToken for Amadeus stats, automatidally goes to get safety stats
   getPollution(lat, lon); //Get air pollution data
   getRiskData(lat, lon); //Get crime rate data
-  localStorage.setItem("latitude",lat);
-  localStorage.setItem("longitude",lon) 
-  modalDialog()
-  $( "#City-Modal").dialog( "option", "title", cityName)
+  localStorage.setItem("latitude", lat);
+  localStorage.setItem("longitude", lon);
+  $("#City-Modal").dialog("option", "title", cityName);
+  $(".ui-dialog").css("display", "block");
 });
-
- function modalDialog() {
-  $( "#City-Modal" ).dialog();
-} ;
 
 // GET AUTHORIZATION TOKEN FOR AMADEUS, THEN GET SAFETY STATS
 function getAuthToken() {
@@ -82,8 +83,8 @@ function getAuthToken() {
     method: "POST",
     body: new URLSearchParams({
       grant_type: "client_credentials",
-      client_id: "AH16AW42PJimckbRBSsWHMEdxRK5Sw5e",
-      client_secret: "y23o1seCk1n1jLfh",
+      client_id: "fVizsTLL8V8dE2jGcoq8mj1ETYQ87DY3",
+      client_secret: "B7vAx8AQGk1gHpz7",
     }),
   })
     .then(function (response) {
@@ -114,28 +115,43 @@ function getSafetyData(lat, lon, token) {
         return response.json();
       })
       .then(function (data) {
-        console.log(data)
         if (data.data) {
-        
-
-          $("#overall").text("Overall safety score: "+ categorizeData(data.data[0].safetyScores.overall))
-          $("#lgbtq").text("Harm to LGBTQ people "+ categorizeData(data.data[0].safetyScores.lgbtq))
-          $("#medical").text("Illness: "+categorizeData(data.data[0].safetyScores.medical))
-          $("#women").text("Harm to women: "+categorizeData(data.data[0].safetyScores.women))
-          $("#poliFreedom").text("Politcal unrest: "+categorizeData(data.data[0].safetyScores.politicalFreedom))
-          console.log(data.data[0].safetyScores);
-
+          $(".likelihoodHeader").text("Likelihood of harm:");
+          $(".safetyStats").css("display", "block");
+          $("#overall").text(
+            "Overall safety score: " +
+              categorizeData(data.data[0].safetyScores.overall)
+          );
+          $("#lgbtq").text(
+            "Harm to LGBTQ people: " +
+              categorizeData(data.data[0].safetyScores.lgbtq)
+          );
+          $("#medical").text(
+            "Illness: " + categorizeData(data.data[0].safetyScores.medical)
+          );
+          $("#women").text(
+            "Harm to women: " + categorizeData(data.data[0].safetyScores.women)
+          );
+          $("#poliFreedom").text(
+            "Politcal unrest: " +
+              categorizeData(data.data[0].safetyScores.politicalFreedom)
+          );
         } else {
-          console.log("No data available for this location");
+          $(".likelihoodHeader").text("");
+          $("#overall").text("No safety data available for this location");
+          $("#lgbtq").text("");
+          $("#medical").text("");
+          $("#women").text("");
+          $("#poliFreedom").text("");
         }
       });
   }
 }
 
-function categorizeData(num){
-  var categories= ["Very Low", "Low", "Moderate", "High", "Very High"]
-  var index = Math.floor(num/20)
-  return categories[index]
+function categorizeData(num) {
+  var categories = ["Very Low", "Low", "Moderate", "High", "Very High"];
+  var index = Math.floor(num / 20);
+  return categories[index];
 }
 
 // GET AIR POLLUTION FOR AN AREA
@@ -145,20 +161,19 @@ function getPollution(lat, lon) {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      // console.log(data);
 
-
-//ACTUAL FUNCTIONALITY BELOW//
-      if (data.list[0].main.aqi === 1) {     
-        $('#air').text("Air Quality: Good")
+      //ACTUAL FUNCTIONALITY BELOW//
+      if (data.list[0].main.aqi === 1) {
+        $("#air").text("Air Quality: Good");
       } else if (data.list[0].main.aqi === 2) {
-        $('#air').text("Air Quality: Fair")
+        $("#air").text("Air Quality: Fair");
       } else if (data.list[0].main.aqi === 3) {
-        $('#air').text("Air Quality: Moderate")
+        $("#air").text("Air Quality: Moderate");
       } else if (data.list[0].main.aqi === 4) {
-        $('#air').text("Air Quality: Poor")
+        $("#air").text("Air Quality: Poor");
       } else if (data.list[0].main.aqi === 5) {
-        $('#air').text("Air Quality: Very Poor")
+        $("#air").text("Air Quality: Very Poor");
       }
     });
 }
@@ -168,8 +183,7 @@ function getRiskData(data) {
   fetch("https://api.precisely.com/oauth/token", {
     method: "POST",
     headers: {
-      Authorization:
-          `Basic RjdzUEduaEFSR1Q1WEpsbWo3a29CdE5PMGtSUFZhdVo6dE9LMFZsamdjZm5kVjl6dA`,
+      Authorization: `Basic RjdzUEduaEFSR1Q1WEpsbWo3a29CdE5PMGtSUFZhdVo6dE9LMFZsamdjZm5kVjl6dA`,
       "Content-Type": "application/x-www-form-urlencoded",
     },
     body: "grant_type=client_credentials",
@@ -179,13 +193,13 @@ function getRiskData(data) {
     })
     .then(function (data) {
       riskResponse(data);
-      console.log(data);
+      // console.log(data);
     });
 }
 
 // GET CRIME RISK DATA FOR AN AREA
 function riskResponse(data) {
-  var riskToken =data.access_token
+  var riskToken = data.access_token;
   fetch(
     `https://api.precisely.com/risks/v1/crime/bylocation?latitude=${lat}8&longitude=${lon}&type=all&includeGeometry=N`,
     {
@@ -201,44 +215,54 @@ function riskResponse(data) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      console.log(data.themes[0].crimeIndexTheme.indexVariable[0].category);
-      var crimeCategory =
-        data.themes[0].crimeIndexTheme.indexVariable[0].category;
+      if (data) {
+        // console.log(data);
+        // console.log(data.themes[0].crimeIndexTheme.indexVariable[0].category);
+        var crimeCategory =
+          data.themes[0].crimeIndexTheme.indexVariable[0].category;
 
-      console.log(data.themes[0].crimeIndexTheme.indexVariable[0].percentile);
-      var crimePercentile =
-        data.themes[0].crimeIndexTheme.indexVariable[0].percentile;
-     
-        $("#percentile").text("Crime Rate Percentile: "+ data.themes[0].crimeIndexTheme.indexVariable[0].percentile);
+        // console.log(data.themes[0].crimeIndexTheme.indexVariable[0].percentile);
+        var crimePercentile =
+          data.themes[0].crimeIndexTheme.indexVariable[0].percentile;
+
+        $("#percentile").text(
+          "Crime Rate Percentile: " +
+            data.themes[0].crimeIndexTheme.indexVariable[0].percentile
+        );
+      } else {
+        $("#percentile").text("No crime rate data for this area");
+      }
     });
 }
 
-// Favorite Button Allows User to Save Searches to Local Storage 
-$('.faveBtn').click(function(event){
-  console.log("hello");
+// Favorite Button Allows User to Save Searches to Local Storage
+$(".faveBtn").click(function (event) {
   event.preventDefault;
 
-  console.log(lat)
-  console.log(lon)
-  
-  localStorage.setItem("latitude north",mapNorth);
-  localStorage.setItem("latitude south",mapSouth);
-  localStorage.setItem("longitude east",mapEast);
-  localStorage.setItem("longitude west",mapWest)
-  
-})
+  localStorage.setItem("latitude north", mapNorth);
+  localStorage.setItem("latitude south", mapSouth);
+  localStorage.setItem("longitude east", mapEast);
+  localStorage.setItem("longitude west", mapWest);
+});
 
 // Retrieving from Local Storage and Displaying on UI
 
-if(localStorage.getItem("latitude north") && localStorage.getItem("latitude south") && localStorage.getItem("longitude east") && localStorage.getItem("longitude west")){
+if (
+  localStorage.getItem("latitude north") &&
+  localStorage.getItem("latitude south") &&
+  localStorage.getItem("longitude east") &&
+  localStorage.getItem("longitude west")
+) {
+  var lsNorth = parseFloat(localStorage.getItem("latitude north"));
+  var lsSouth = parseFloat(localStorage.getItem("latitude south"));
+  var lsEast = parseFloat(localStorage.getItem("longitude east"));
+  var lsWest = parseFloat(localStorage.getItem("longitude west"));
   const bounds = [
-  [localStorage.getItem("longitude west"), localStorage.getItem("latitude south")],
-  [localStorage.getItem("longitude east"),localStorage.getItem("latitude north") ],
-  ]
-  var center = [localStorage.getItem("longitude west") + localStorage.getItem("longitude east") / 2, localStorage.getItem("latitude north") + localStorage.getItem("latitude south")/2];
+    [lsWest, lsSouth],
+    [lsEast, lsNorth],
+  ];
+  var center = [(lsEast + lsWest) / 2, (lsNorth + lsSouth) / 2];
   // Reset the map bounds and pan to it
   map.setMaxBounds(bounds);
-  map.panTo(center);};
-
-
+  map.panTo(center);
+}
